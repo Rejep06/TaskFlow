dad494b8a1bb53da08125832fa6e9c9cf984f221
RealSigma1 <inalshidakov8@gmail.com>
2025-12-07 21:44:20 +0300

    add task.hpp, based class of tasks

--- core/task.hpp
+++ core/task.hpp
@@ -2,12 +2,49 @@
 #include <string>
 
 class Task {
-   private:
-    int id;
-    std::string title;
-    bool done;
+public:
+    Task(int id,
+         const std::string& title,
+         const std::string& description,
+         bool completed = false)
+        : id_(id)
+        , title_(title)
+        , description_(description)
+        , completed_(completed) {
+    }
 
-   public:
-    Task(int id = 0, const std::string& title = "", bool done = false)
-        : id(id), title(title), done(done) {}
+    int getId() const {
+        return id_;
+    }
+
+    const std::string& getTitle() const {
+        return title_;
+    }
+
+    void setTitle(const std::string& title) {
+        title_ = title;
+    }
+
+    const std::string& getDescription() const {
+        return description_;
+    }
+
+    void setDescription(const std::string& description) {
+        description_ = description;
+    }
+
+    bool isCompleted() const {
+        return completed_;
+    }
+
+    void setCompleted(bool completed) {
+        completed_ = completed;
+    }
+
+private:
+    int id_;
+    std::string title_;
+    std::string description_;
+    bool completed_;
 };
+    
