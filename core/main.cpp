dcb267e6f7d9c98213adc30c6777bd84dfad49b6
RealSigma1 <inalshidakov8@gmail.com>
2025-12-09 22:18:17 +0300

    add deadlines and time tracker

--- core/main.cpp
+++ core/main.cpp
@@ -1,7 +1,20 @@
-#include "task_manager.hpp"
+#include <windows.h>
+#include <iostream>
+#include <locale>
 #include "console_app.hpp"
 
+#include "task_manager.hpp"
+
 int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    try {
+        std::locale::global(std::locale("ru_RU.UTF-8"));
+    } catch (...) {
+        
+    }
+
     TaskManager manager;
     ConsoleApp app(manager);
     app.run();
