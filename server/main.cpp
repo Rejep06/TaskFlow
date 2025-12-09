16135378e8117407f95097d3ce5227ba3cc62e59
Rejep <rsemsetdinov@gmail.com>
2025-12-09 21:17:59 +0500

    Add TaskController and TaskRepository

--- server/main.cpp
+++ server/main.cpp
@@ -1,9 +1,24 @@
+#include <httplib.h>
+
 #include <iostream>
-#include "../core/task.hpp"
-#include "../core/task_manager.cpp"
+
+#include "TaskController.hpp"
+#include "TaskManager.hpp"
+#include "TaskRepository.hpp"
 
 int main() {
-    addTask("First task");
-    std::cout << "Server started\n";
-    return 0;
+    TaskRepository repo;
+    TaskManager manager;
+
+    auto tasks = repo.load();
+    for (const auto& t : tasks) {
+        manager.addTask(t);
+    }
+
+    httplib::Server server;
+    TaskController controller(manager, repo);
+    controller.registerRoutes(server);
+
+    std::cout << "Server started on http://localhost:8080\n";
+    server.listen("0.0.0.0", 8080);
 }
