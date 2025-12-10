4dbb5676bad03c1bdd7e54ccb1e668216602ca7b
Rejep <rsemsetdinov@gmail.com>
2025-12-10 05:15:22 +0500

    Add parser and change to dynamic class
    Change server

--- server/TaskRepository.hpp
+++ server/TaskRepository.hpp
@@ -1,12 +1,17 @@
 #pragma once
+#include <memory>
 #include <vector>
+
 #include "task.hpp"
+#include "utils/ParserTime.hpp"
 
 class TaskRepository {
-public:
+   public:
+    TaskRepository(std::shared_ptr<ParserTime> p);
     std::vector<Task> load();
     void save(const std::vector<Task>& tasks);
 
-private:
+   private:
+    std::shared_ptr<ParserTime> parserTime;
     const std::string filename = "data/tasks.json";
 };
--- server/main.cpp
+++ server/main.cpp
@@ -3,21 +3,23 @@
 #include <iostream>
 
 #include "TaskController.hpp"
-#include "TaskManager.hpp"
 #include "TaskRepository.hpp"
+#include "task_manager.hpp"
 
 int main() {
-    TaskRepository repo;
-    TaskManager manager;
+    std::shared_ptr<ParserTime> parserTime = std::make_shared<ParserTime>();
+    std::shared_ptr<ITaskService> manager = std::make_shared<TaskManager>();
+    std::shared_ptr<TaskRepository> repo = std::make_shared<TaskRepository>(parserTime);
 
-    auto tasks = repo.load();
+    auto tasks = repo->load();
     for (const auto& t : tasks) {
-        manager.addTask(t);
+        manager->addTask(t);
     }
 
     httplib::Server server;
-    TaskController controller(manager, repo);
-    controller.registerRoutes(server);
+    std::shared_ptr<TaskController> controller =
+        std::make_shared<TaskController>(manager, repo, parserTime);
+    controller->registerRoutes(server);
 
     std::cout << "Server started on http://localhost:8080\n";
     server.listen("0.0.0.0", 8080);
