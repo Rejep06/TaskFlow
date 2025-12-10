ac2de04efc1d408e367cc943358b1c8033c0fc7b
Rejep <rsemsetdinov@gmail.com>
2025-12-10 05:15:22 +0500

    Add parser and change to dynamic class
    Change server

--- server/TaskController.cpp
+++ server/TaskController.cpp
@@ -1,43 +1,66 @@
 #include "TaskController.hpp"
 
+#include <iostream>
 #include <nlohmann/json.hpp>
+
+#include "ParserTime.hpp"
 
 using json = nlohmann::json;
 
-TaskController::TaskController(ITaskService& m, TaskRepository& r)
-    : manager(m), repo(r) {}
+
+TaskController::TaskController(std::shared_ptr<ITaskService> m,
+                               std::shared_ptr<TaskRepository> r,
+                               std::shared_ptr<ParserTime> p)
+    : manager(m), repo(r), parserTime(p) {
+}
 
 void TaskController::registerRoutes(httplib::Server& server) {
     server.Get("/tasks", [&](const httplib::Request&, httplib::Response& res) {
-        auto tasks = manager.getAllTasks();
+        auto tasks = manager->getAllTasks();
         json j;
 
         for (const auto& t : tasks) {
-            j.push_back({{"id", t.getId()},
-                         {"title", t.getTitle()},
-                         {"description", t.getDescription()},
-                         {"deadline", t.getDeadline()},
-                         {"completed", t.isCompleted()}});
+            json item = {
+                {"id", t.getId()},
+                {"title", t.getTitle()},
+                {"description", t.getDescription()},
+                {"deadline", parserTime->timePointToString(t.getDeadline())},
+                {"completed", t.isCompleted()}};
+            j.push_back(item);
         }
 
         res.set_content(j.dump(), "application/json");
     });
 
     server.Post("/tasks", [&](const httplib::Request& req, httplib::Response& res) {
+        json body;
+
         try {
-            auto body = json::parse(req.body);
-
-            Task task = manager.createTask(
-                body["title"].get<std::string>(),
-                body["description"].get<std::string>());
-
-            repo.save(manager.getAllTasks());  // надо поменять на чтобы сохранял все задачи по необходимости
-
-            res.set_content("{\"status\":\"ok\"}", "application/json");
-        } catch (std::exception& e) {
-            res.status = 500;
-            res.set_content(std::string("{\"error\":\"") + e.what() + "\"}", "application/json");
-            std::cerr << "POST /tasks error: " << e.what() << std::endl;
+            body = json::parse(req.body);
+        } catch (...) {
+            res.status = 400;
+            res.set_content("{\"error\":\"invalid json\"}", "application/json");
+            return;
         }
+
+        if (!body.contains("title") || !body.contains("description")) {
+            res.status = 400;
+            res.set_content("{\"error\":\"Missing fields\"}", "application/json");
+            return;
+        }
+
+        std::optional<std::chrono::system_clock::time_point> deadline;
+
+        if (body.contains("deadline"))
+            deadline = parserTime->stringToTimePoint(body["deadline"].get<std::string>());
+
+        Task& task = manager->createTask(
+            body["title"].get<std::string>(),
+            body["description"].get<std::string>(),
+            deadline);
+
+        repo->save(manager->getAllTasks());
+
+        res.set_content("{\"status\":\"ok\"}", "application/json");
     });
 }
--- server/TaskController.hpp
+++ server/TaskController.hpp
@@ -1,15 +1,19 @@
 #pragma once
 #include <httplib.h>
 
+#include <memory>
+
 #include "ITaskService.hpp"
+#include "ParserTime.hpp"
 #include "TaskRepository.hpp"
 
 class TaskController {
    public:
-    TaskController(ITaskService& manager, TaskRepository& repo);
+    TaskController(std::shared_ptr<ITaskService> m, std::shared_ptr<TaskRepository> r, std::shared_ptr<ParserTime> p);
     void registerRoutes(httplib::Server& server);
 
    private:
-    ITaskService& manager;
-    TaskRepository& repo;
+    std::shared_ptr<ITaskService> manager;
+    std::shared_ptr<TaskRepository> repo;
+    std::shared_ptr<ParserTime> parserTime;
 };
